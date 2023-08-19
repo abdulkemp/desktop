f10cba007667cd2bc4cfc2a255f3531cd682b0c0
Abdul Azeez Kemp <aakemp786@gmail.com>
2023-08-19 13:09:02 +0200

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -431,3 +431,19 @@ p {
 <style lang="scss" scoped>
 
 </style>
+<!-- 
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style> -->
